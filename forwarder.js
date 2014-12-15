9d8cd7e30a4cb108c084c9f79f875799955dc492
Kristof Coomans <kristof@coomans.eu>
2014-12-15 11:23:55 +0100

    Configure exchange and queue names

--- forwarder.js
+++ forwarder.js
@@ -5,32 +5,34 @@ var jsonlite = require(__dirname + '/jsonlite.js');
 var Pusher = require('pusher-client');
 var pusher = new Pusher(config['pusher']['application_key']);
 
-var connect = require('amqp').createConnection(config['amqp_connection']);
+var connect = require('amqp').createConnection(config['amqp']['connection']);
 
+console.log("subscribing to Pusher channel " + config['pusher']['channel']);
 var pusher_channel = pusher.subscribe(config['pusher']['channel']);
 
 connect.on('ready', function () {
   console.log('connect ready');
   var exchange = connect.exchange(
-      'sapi2',
+      config['amqp']['exchange'],
       {
         type: 'direct',
         durable: true,
         autoDelete: false
       }
   );
-  var q = connect.queue('udb3-sapi2', {
+  console.log('declaring queue ' + config['amqp']['queue']);
+
+  var q = connect.queue(config['amqp']['queue'], {
     passive: false,
     durable: true,
     autoDelete: false,
     exclusive: false
   });
+
   q.on('queueDeclareOk', function (args) {
     console.log('queue declared');
-    // our udb3-sapi2 queue will only receive 'solr_update' messages from
-    // the 'sapi2' exchange
-    q.bind('sapi2', 'solr_update');
-    q.on('queueBindOk', function () {
+    console.log('binding queue to exchange ' + config['amqp']['exchange'] + ' with routing key solr_update');
+    q.bind(config['amqp']['exchange'], 'solr_update', function() {
       pusher_channel.bind('solr_update', function (data) {
         var message = jsonlite.parse(data);
 
